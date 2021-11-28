4c6d1ac2677d55a0c84d45b025b4e2f6251fca2b
Yann Figueiredo <yann.fabricio@hotmail.com>
2021-11-27 21:30:38 -0300

    Iniadas funções para implementar janela modal

---
+++ js/galeria-modal.js
@@ -0,0 +1,8 @@
+function gerenciarGaleriaModal(config){
+    this.modal = document.querySelector(config.modal);
+    this.imgGaleria = document.querySelectorAll(config.imgGaleria);
+
+    function atribuirImg(){
+        alert('ola');
+    }
+}
--- js/main.js
+++ js/main.js
@@ -9,4 +9,13 @@
         itens: '.menu-servicos li',
         imgGaleria: '.cards-servicos figure'
     });
+
+    var galeriaModal = gerenciarGaleriaModal({
+        modal: '.modal',
+        imgGaleria: '.cards-servicos figure img',
+        btnVoltar: '#btn-voltar',
+        btnAvancar: '#btn-avancar',
+        btnFechar: '#btn-fechar',
+        controladorSlide: '.controlador-slide'
+    });
 }());
